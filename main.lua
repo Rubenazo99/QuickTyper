220f7d04f241b67b3562b457c68a58fb8a2e0841
Ruben Lopez <rlopezg@edu.tecnocampus.cat>
2022-11-15 18:30:28 +0100

    commit

---
+++ main.lua
@@ -0,0 +1,18 @@
+--[[
+    Your love2d game start here
+]]
+
+love.graphics.setDefaultFilter('nearest', 'nearest')
+
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
+
