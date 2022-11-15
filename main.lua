98b936343a6f584d45a3de0adc124adaec66a89f
Eduard Sales <EvilHat2@gmail.com>
2022-11-15 20:27:57 +0100

    Dubjos y audio

--- main.lua
+++ main.lua
@@ -1,18 +1,25 @@
 --[[
     Your love2d game start here
 ]]
+local w, h = love.graphics.getDimensions()
 
 love.graphics.setDefaultFilter('nearest', 'nearest')
 
 function love.load()
-
+    love.filesystem.load("src/RenderSprites.lua")()
+    love.filesystem.load("src/Audios.lua")()
+    
+    audioLoad()
+    spriteLoad()
 end
 
 function love.update(dt)
-
+    audioLoad()
 end
 
 function love.draw()
+
+    spriteDraw() --Se dibuja tanto el fondo como la mesa de escribir
 
 end
 
